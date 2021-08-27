a9a430e0bc1ab72c9e264ed67ef023aceb207e4e
sharadgupta29 <sharadg399@gmail.com>
2021-08-28 02:30:05 +0530

    first commit with basic setup

---
+++ src/test/java/com/nocompany/spendingtracker/SpendingTracker/SpendingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocompany.spendingtracker.SpendingTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
